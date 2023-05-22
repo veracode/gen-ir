2265f9a7da70bb64c7437d5fe7056326322314d4
NinjaLikesCheez <NinjaLikesCheez@users.noreply.github.com>
2023-05-22 13:59:46 +0200

    Add test for MultipleApp

---
+++ Tests/GenIRTests/MultipleAppTests.swift
@@ -0,0 +1,29 @@
+import XCTest
+@testable import gen_ir
+import PBXProjParser
+
+final class MultipleAppTests: XCTestCase {
+	static private var testPath: URL = {
+		TestContext.testAssetPath
+			.appendingPathComponent("MultipleApp")
+			.appendingPathComponent("MultipleApp.xcodeproj")
+	}()
+
+	func testExpectedTargetLookup() throws {
+		let context = try TestContext()
+		let result = try context.build(test: Self.testPath, scheme: "MultipleApp")
+
+		let project: ProjectParser = try ProjectParser(path: Self.testPath, logLevel: .debug)
+		var targets = Targets(for: project)
+
+		let logContents = try String(contentsOf: context.buildLog).components(separatedBy: .newlines)
+		let log = XcodeLogParser(log: logContents)
+		try log.parse(&targets)
+
+		let app = try XCTUnwrap(targets.target(for: "MultipleApp"))
+		let copy = try XCTUnwrap(targets.target(for: "MultipleApp Copy"))
+
+		XCTAssertEqual(app.commands.count, 3)
+		XCTAssertEqual(copy.commands.count, 3)
+	}
+}
