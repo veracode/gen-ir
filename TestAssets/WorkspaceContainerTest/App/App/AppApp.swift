76b023ca0c7286de784c0a46db298656a2c2c8b1
NinjaLikesCheez <NinjaLikesCheez@users.noreply.github.com>
2023-09-04 11:56:39 +0200

    Add test for container tags in xcworkspaces

---
+++ TestAssets/WorkspaceContainerTest/App/App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Thomas Hedderwick on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestAssets/WorkspaceContainerTest/Modules/FrameworkB/FrameworkB/FrameworkB.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkB.h
+//  FrameworkB
+//
+//  Created by Thomas Hedderwick on 04/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkB.
+FOUNDATION_EXPORT double FrameworkBVersionNumber;
+
+//! Project version string for FrameworkB.
+FOUNDATION_EXPORT const unsigned char FrameworkBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkB/PublicHeader.h>
+
+
