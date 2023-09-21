6601453605a470ec8fa9577e1a0fb4371841e62c
NinjaLikesCheez <NinjaLikesCheez@users.noreply.github.com>
2023-09-21 18:21:17 +0200

    Rework file copying to match file sizes to stop copying of the same file

--- Sources/GenIR/OutputPostprocessor.swift
+++ Sources/GenIR/OutputPostprocessor.swift
@@ -12,7 +12,7 @@ import PBXProjParser
 /// The `CompilerCommandRunner` will output IR with it's product name, but doesn't take into account the linking of products into each other.
 /// The `OutputPostprocessor` attempts to remedy this by using the `ProjectParser` to detect dependencies between products AND
 /// parsing the `xcarchive` to determine if something was statically linked, and if so, copies the IR for that product into the linker's IR folder.
-struct OutputPostprocessor {
+class OutputPostprocessor {
 	/// The to the output IR folders that will be processed
 	let output: URL
 
@@ -20,6 +20,8 @@ struct OutputPostprocessor {
 	private let dynamicDependencyToPath: [String: URL]
 
 	private let graph: DependencyGraph
+
+	private var seenConflictingFiles: [URL: [(String, Int)]] = [:]
 
 	init(archive: URL, output: URL, targets: Targets) throws {
 		self.output = output
@@ -98,7 +100,8 @@ struct OutputPostprocessor {
 
 				// Move the dependency IR (the node) to the depender (the thing depending on this node)
 				do {
-					try FileManager.default.copyItemMerging(at: nodeFolderPath, to: dependerFolderPath, replacing: true)
+					try copyDirectoryMergingDifferingFiles(at: nodeFolderPath, to: dependerFolderPath)
+					// try FileManager.default.copyItemMerging(at: nodeFolderPath, to: dependerFolderPath, replacing: true)
 				} catch {
 					logger.debug("Copy error: \(error)")
 				}
@@ -107,6 +110,72 @@ struct OutputPostprocessor {
 		}
 
 		return processed
+	}
+
+	// TODO: rework this file to not have side effects and make it more readable...
+	func copyDirectoryMergingDifferingFiles(at source: URL, to destination: URL) throws {
+		let manager = FileManager.default
+		let files = try manager.contentsOfDirectory(at: source, includingPropertiesForKeys: nil)
+
+		func size(for path: URL) throws -> Int? {
+			do {
+				return try manager.attributesOfItem(atPath: path.filePath)[.size] as? Int
+			} catch {
+				logger.debug("Couldn't get size attribute for path: \(path.filePath)")
+				throw error
+			}
+		}
+
+		let (existing, nonexisting) = files
+			.map {
+				(source: source.appendingPathComponent($0.lastPathComponent), destination: destination.appendingPathComponent($0.lastPathComponent))
+			}
+			.reduce(into: (existing: [(source: URL, destination: URL)](), nonexisting: [(source: URL, destination: URL)]())) { (partialResult, sourceAndDestination) in
+				if manager.fileExists(atPath: sourceAndDestination.destination.filePath) {
+					partialResult.existing.append(sourceAndDestination)
+				} else {
+					partialResult.nonexisting.append(sourceAndDestination)
+				}
+			}
+
+		// Files that don't already exist at the destination can be moved over easily
+		try nonexisting
+			.forEach { (source, destination) in
+				try manager.copyItem(at: source, to: destination)
+			}
+
+		// Files that do exist require some additional checks, and potentially renaming
+		for (sourceURL, destinationURL) in existing {
+			guard
+				let destinationSize = try size(for: destinationURL),
+				let sourceSize = try size(for: sourceURL)
+			else {
+				continue
+			}
+
+			if sourceSize == destinationSize {
+				// Ignore the file
+				logger.debug("Ignoring copy of: \(destinationURL.lastPathComponent) as the sizes are the same")
+				continue
+			}
+
+			// Unique the file name
+			let uniqueDestinationURL = manager.uniqueFilename(directory: destination, filename: sourceURL.lastPathComponent)
+
+			// TODO: Should we use all file attributes here? What about created date etc?
+			if seenConflictingFiles[sourceURL] == nil {
+				seenConflictingFiles[sourceURL] = [(sourceURL.lastPathComponent, sourceSize)]
+			}
+
+			for (_, size) in seenConflictingFiles[sourceURL]! where size == destinationSize {
+				logger.debug("Ignoring copy of: \(destinationURL.lastPathComponent) as the sizes where the same")
+				continue
+			}
+
+			seenConflictingFiles[sourceURL]?.append((uniqueDestinationURL.lastPathComponent, destinationSize))
+
+			try manager.copyItem(at: sourceURL, to: uniqueDestinationURL)
+		}
 	}
 }
 
