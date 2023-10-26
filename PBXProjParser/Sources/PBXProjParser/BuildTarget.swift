96fd6a829b89e51cfe4f183ce1c379216aa4b482
Kevin Rise <krise@veracode.com>
2023-10-26 12:16:54 -0400

    space to tabs

--- PBXProjParser/Sources/PBXProjParser/BuildTarget.swift
+++ PBXProjParser/Sources/PBXProjParser/BuildTarget.swift
@@ -12,57 +12,57 @@ import Foundation
 
 // TODO: this enum is shared by both this and the TARGET file parser - combine/do-better
 public enum TargetType: CustomStringConvertible {
-        case Application
-        case Framework
-        case Bundle
-        case Extension
-        case Unknown
+		case Application
+		case Framework
+		case Bundle
+		case Extension
+		case Unknown
 
-        public var description: String {
-            switch self {
-                case .Application: return "Application"
-                case .Framework: return "Framework"
-                case .Bundle: return "Bundle"
-                case .Extension: return "Extension"
-                default: return "Unknown"
-            }
-        }
-    }
+		public var description: String {
+			switch self {
+				case .Application: return "Application"
+				case .Framework: return "Framework"
+				case .Bundle: return "Bundle"
+				case .Extension: return "Extension"
+				default: return "Unknown"
+			}
+		}
+	}
 
 /// Represents a build target we need to process
 public struct BuildTarget {
 
-    public let name: String
-    let productName: String
-    let frPath: String
-    public let type: TargetType
+	public let name: String
+	let productName: String
+	let frPath: String
+	public let type: TargetType
 
-    public init(name: String, productName: String, fileRef: PBXFileReference) {
-        self.name = name
-        self.productName = productName
-        self.frPath = fileRef.path
-        self.type = Self.getType(typeName: fileRef.explicitFileType)
+	public init(name: String, productName: String, fileRef: PBXFileReference) {
+		self.name = name
+		self.productName = productName
+		self.frPath = fileRef.path
+		self.type = Self.getType(typeName: fileRef.explicitFileType)
 
-    }
+	}
 
-    // public var description: String {
-    //     return "{name: \(name), productName: \(productName), type: \(type), fileRefPath: \(frPath)}"
-    // }
-    
-    private static func getType(typeName: String?) -> TargetType{
-        switch typeName {
-            case "wrapper.application":
-                return TargetType.Application
-            case "wrapper.framework":
-                return TargetType.Framework
-            case "wrapper.cfbundle":
-                return TargetType.Bundle
-            case "wrapper.app-extension":
-                return TargetType.Extension
-            default:
-                return TargetType.Unknown
-        }
-    }
+	// public var description: String {
+	//     return "{name: \(name), productName: \(productName), type: \(type), fileRefPath: \(frPath)}"
+	// }
+	
+	private static func getType(typeName: String?) -> TargetType{
+		switch typeName {
+			case "wrapper.application":
+				return TargetType.Application
+			case "wrapper.framework":
+				return TargetType.Framework
+			case "wrapper.cfbundle":
+				return TargetType.Bundle
+			case "wrapper.app-extension":
+				return TargetType.Extension
+			default:
+				return TargetType.Unknown
+		}
+	}
 
 
 }
